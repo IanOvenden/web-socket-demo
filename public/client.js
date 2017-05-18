80f06e79903176345c0ac63dab7243f573f350b1
Ian Ovenden <iovenden@wiley.com>
2017-05-18 16:44:33 +0100

    added task title update functionality

--- public/client.js
+++ public/client.js
@@ -15,6 +15,10 @@ socket.onmessage = function (event) {
 	switch(payload.type) {
 		case 'list-title':
 			document.querySelector('#list-title-' + payload.id).value = payload.message;
+			break;
+		case 'task-title':
+			document.querySelector('#task-title-' + payload.id).value = payload.message;
+			break;
 	}
 }
 
@@ -46,6 +50,22 @@ for (var i = 0; i < listTitles.length; i++) {
 		console.log('Sent: ' + json);
     });
 }
+
+
+var taskTitles = document.querySelectorAll('.task-title');
+
+for (var i = 0; i < taskTitles.length; i++) {
+    taskTitles[i].addEventListener('change', function(event) {
+        var json = JSON.stringify({ 
+			type: 'task-title',
+			id: this.id.replace('task-title-',''),
+			message: this.value 
+		});
+		socket.send(json);
+		console.log('Sent: ' + json);
+    });
+}
+
 
 var log = function(text) {
 	var li = document.createElement('li');
